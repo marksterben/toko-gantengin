8436febf347b030702c38a7b7a605ea61d7132d7
marksterben <68014273+marksterben@users.noreply.github.com>
2021-07-25 11:52:08 +0700

    create product model, factory, seeder, migration

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word(),
+            'description' => $this->faker->text(300),
+            'price' => $this->faker->randomNumber(5, true),
+            'status' => 'available',
+        ];
+    }
+}
