1e8e50a4b72359d124bb5b4f6de5fc196aba123d
marksterben <68014273+marksterben@users.noreply.github.com>
2021-07-25 14:05:29 +0700

    fix bugs in product migration

--- database/migrations/2021_07_25_035822_create_products_table.php
+++ database/migrations/2021_07_25_035822_create_products_table.php
@@ -16,9 +16,9 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->integer('price');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->enum('status', ['available', 'unavailable']);
             $table->timestamps();
         });
