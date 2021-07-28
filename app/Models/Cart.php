5c8710e69fad3a8242f160b45f33a836517a2ed3
marksterben <68014273+marksterben@users.noreply.github.com>
2021-07-28 21:17:29 +0700

    create cart migration

---
+++ app/Models/Cart.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * Get the user that owns the cart.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the product that owns the cart.
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
