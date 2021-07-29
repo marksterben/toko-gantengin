9ee8f76f32cbed0c3c9f8caacd7e2e64e284fcff
marksterben <68014273+marksterben@users.noreply.github.com>
2021-07-29 20:27:40 +0700

    create model and migration for order and orderdetail

---
+++ app/Models/OrderDetail.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderDetail extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * Get the product that owns the order.
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
---
+++ database/migrations/2021_07_29_125046_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->integer('total');
+            $table->string('recipient');
+            $table->text('address');
+            $table->string('phone');
+            $table->enum('status', ['waiting', 'paid', 'finished', 'canceled']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
