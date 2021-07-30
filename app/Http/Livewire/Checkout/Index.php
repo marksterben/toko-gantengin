cb22912ad21500285f7d58a27d691b83218e4e3a
marksterben <68014273+marksterben@users.noreply.github.com>
2021-07-30 10:42:29 +0700

    create checkout feature

--- app/Http/Livewire/Checkout/Index.php
+++ app/Http/Livewire/Checkout/Index.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Livewire\Checkout;
 
+use App\Models\Order;
+use App\Models\OrderDetail;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Illuminate\Support\Str;
 
 class Index extends Component
 {
@@ -26,5 +29,35 @@ class Index extends Component
     public function render()
     {
         return view('livewire.checkout.index')->extends('layouts.app');
+    }
+
+    public function checkout()
+    {
+        $this->validate();
+
+        $order = Order::create([
+            'invoice' => Str::random(4) . date('dmy'),
+            'user_id' => Auth::id(),
+            'total' => $this->carts->sum('subtotal'),
+            'recipient' => $this->recipient,
+            'address' => $this->address,
+            'phone' => $this->phone,
+            'status' => 'waiting',
+        ]);
+
+        if ($order) {
+            foreach ($this->carts as $cart) {
+                OrderDetail::create([
+                    'order_id' => $order->id,
+                    'product_id' => $cart->product_id,
+                    'qty' => $cart->qty,
+                    'subtotal' => $cart->subtotal,
+                ]);
+
+                $cart->delete();
+            }
+
+            return redirect('/');
+        }
     }
 }
--- resources/views/livewire/checkout/index.blade.php
+++ resources/views/livewire/checkout/index.blade.php
@@ -1,69 +1,69 @@
- <div class="container py-4">
-     <div class="row">
-         <div class="col-md-8">
-             <div class="card">
-                 <div class="card-header">Formulir Alamat Pengirimian</div>
-                 <div class="card-body">
-                     <form>
-                         <div class="mb-3">
-                             <label for="recipient" class="form-label">Nama</label>
-                             <input wire:model="recipient" type="text" class="form-control" id="recipient"
-                                 placeholder="Masukkan nama penerima" />
-                             @error('recipient') <span class="small text-danger">{{ $message }}</span> @enderror
-                         </div>
-                         <div class="mb-3">
-                             <label for="address" class="form-label">Alamat</label>
-                             <textarea wire:model="address" id="address" cols="30" rows="5"
-                                 class="form-control"></textarea>
-                             @error('address') <span class="small text-danger">{{ $message }}</span> @enderror
-                         </div>
-                         <div class="mb-3">
-                             <label for="phone" class="form-label">Telepon</label>
-                             <input wire:model="phone" type="text" class="form-control" id="phone"
-                                 placeholder="Masukkan nomor telepon penerima" value="" />
-                             @error('phone') <span class="small text-danger">{{ $message }}</span> @enderror
-                         </div>
-                         <div>
-                             <button type="submit" class="btn btn-primary">Simpan</button>
-                         </div>
-                     </form>
-                 </div>
-             </div>
-         </div>
-         <div class="col-md-4">
-             <div class="row">
-                 <div class="col-md-12">
-                     <div class="card mb-3">
-                         <div class="card-header">Cart</div>
-                         <div class="card-body">
-                             <table class="table">
-                                 <thead>
-                                     <tr>
-                                         <th>Produk</th>
-                                         <th>Qty</th>
-                                         <th>Subtotal</th>
-                                     </tr>
-                                 </thead>
-                                 <tbody>
-                                     @foreach ($carts as $cart)
-                                         <tr>
-                                             <td>{{ $cart->product->name }}</td>
-                                             <td>{{ $cart->qty }}</td>
-                                             <td>Rp {{ number_format($cart->subtotal, 0, ',', '.') }}</td>
-                                         </tr>
-                                     @endforeach
-                                 </tbody>
-                                 <tfoot>
-                                     <tr>
-                                         <th colspan="2">Total</th>
-                                         <th>Rp {{ number_format($carts->sum('subtotal'), 0, ',', '.') }}</th>
-                                     </tr>
-                                 </tfoot>
-                             </table>
-                         </div>
-                     </div>
-                 </div>
-             </div>
-         </div>
-     </div>
- </div>
+<div class="container py-4">
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Formulir Alamat Pengirimian</div>
+                <div class="card-body">
+                    <form wire:submit.prevent="checkout">
+                        <div class="mb-3">
+                            <label for="recipient" class="form-label">Nama</label>
+                            <input wire:model="recipient" type="text" class="form-control" id="recipient"
+                                placeholder="Masukkan nama penerima" />
+                            @error('recipient') <span class="small text-danger">{{ $message }}</span> @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="address" class="form-label">Alamat</label>
+                            <textarea wire:model="address" id="address" cols="30" rows="5"
+                                class="form-control"></textarea>
+                            @error('address') <span class="small text-danger">{{ $message }}</span> @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="phone" class="form-label">Telepon</label>
+                            <input wire:model="phone" type="text" class="form-control" id="phone"
+                                placeholder="Masukkan nomor telepon penerima" value="" />
+                            @error('phone') <span class="small text-danger">{{ $message }}</span> @enderror
+                        </div>
+                        <div>
+                            <button type="submit" class="btn btn-primary">Simpan</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card mb-3">
+                        <div class="card-header">Cart</div>
+                        <div class="card-body">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Produk</th>
+                                        <th>Qty</th>
+                                        <th>Subtotal</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($carts as $cart)
+                                        <tr>
+                                            <td>{{ $cart->product->name }}</td>
+                                            <td>{{ $cart->qty }}</td>
+                                            <td>Rp {{ number_format($cart->subtotal, 0, ',', '.') }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <th colspan="2">Total</th>
+                                        <th>Rp {{ number_format($carts->sum('subtotal'), 0, ',', '.') }}</th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
